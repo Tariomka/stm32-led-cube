7874e70836f4c1571b94125ffe3562268af2732a
Tariomka <arturas.deltuvas@gmail.com>
2024-07-11 19:42:35 +0300

    Easy build

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Starting to compile Tinygo binary, please wait..."
 
-mkdir -p bin
+mkdir -p build
 current_date_time="`date +%Y_%m_%d_%H:%M`";
-tinygo build -o ./bin/main_${current_date_time}.hex -target=bluepill-clone ./src/main.go
+tinygo build -o ./build/main_${current_date_time}.hex -target=bluepill-clone ./src/main.go
 
 echo "Build finished."
